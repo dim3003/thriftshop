80815a4dc8dcee5d17b9291700001cc1f1f0be31
Jey3003 <andre.d3003@gmail.com>
2020-09-21 20:39:56 +0200

    Init seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+item1 = Item.create! title: 'T-shirt',
+                     photo_url: 'tshirt1.jpg',
+                     description: 'A green T-shirt which has a longer description than all of the other stuff.'
+
+
+item1 = Item.create! title: 'T-shirt',
+                    photo_url: 'tshirt2.jpg',
+                    description: 'A plain black T-shirt'
+
+
+item1 = Item.create! title: 'Hoodie',
+                     photo_url: 'hoodie1.jpg',
+                     description: 'A plain hoodie'
