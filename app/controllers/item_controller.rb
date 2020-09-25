06589168e33203c59d00b1d7d950b30099fe5ccf
Jey3003 <andre.d3003@gmail.com>
2020-09-25 13:43:13 +0200

    Save before controller destroy

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -5,11 +5,27 @@ class ItemController < ApplicationController
   end
 
   def create
-
+    @item = Idea.new(item_resource_params)
+    if(@item.save)
+      redirect_to items_path
+    else
+      render 'new'
+    end
   end
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def index
+    @items = Item.all
+  end
+
+
+  private
+
+  def idea_resource_params
+    params.require(:idea).permit(:title, :photo_url, :done_count, :description)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get '/:id/show', to: 'item#show', as: 'item'
 
-  resources :item, only: [:new]
+  resources :items
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
