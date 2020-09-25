dc9cfdb4969cc32dada0e8bce1621f94f00089de
Jey3003 <andre.d3003@gmail.com>
2020-09-25 14:06:52 +0200

    Init controller items

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
 
   get 'home', to: 'home#index'
 
-  get '/:id/show', to: 'item#show', as: 'item'
-
   resources :items
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
