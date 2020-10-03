c3a2393f135e7f27eeba0fe431a2d6c9fe132629
Jey3003 <andre.d3003@gmail.com>
2020-10-03 23:13:48 +0200

    Refactor card with button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,7 @@ class HomeController < ApplicationController
   def index
     @items = Item.all.most_recent
   end
+
+  def attribute
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get 'home', to: 'home#index'
 
+  get 'attribute', to: 'home#attribute'
+
   resources :items
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
