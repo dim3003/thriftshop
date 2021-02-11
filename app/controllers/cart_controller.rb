ae035572f3f7ef059505885430f3e3b55ff63248
Jey3003 <andre.d3003@gmail.com>
2021-02-11 18:00:35 +0100

    order cart in email

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -15,14 +15,28 @@ class CartController < ApplicationController
   end
 
   def confirmation
-    UserMailer.welcome_email.deliver_now
-    
+    @order = Order.new
+    @order.name = params[:name]
+    @order.firstname = params[:firstname]
+    @order.email = params[:email]
+    @order.street = params[:street]
+    @order.postalCode = params[:postalcode]
+    @order.city = params[:city]
+    @order.purchase = params[:cart]
+    @items = Item.all
+    if @order.save
+      UserMailer.with(order: @order, items: @items).welcome_email.deliver_now
+    end
   end
 
   private
 
   def address_resource_params
-    params.require(:address).permit(:name, :firstname, :email, :street, :postalCode)
+    params.require(:address).permit(:name, :firstname, :email, :street, :postalCode, :city)
+  end
+
+  def order_resource_params
+    params.require(:address).permit(:name, :firstname, :email, :street, :postalCode,:city, :purchase)
   end
 
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,8 @@ class UserMailer < ApplicationMailer
   default from: 'thriftshopch@gmail.com'
 
   def welcome_email
-    @url  = '/cart/confirmation'
+    @order = params[:order]
+    @items = params[:items]
     attachments.inline['logo.jpeg'] = File.read('app/assets/images/logo.jpeg')
     mail(to: 'andre.d3003@gmail.com', subject: 'Confirmation de commande')
   end
