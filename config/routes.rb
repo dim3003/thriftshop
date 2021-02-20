05ad801e0560b8d570bbb182c6f8615c449b6c08
Jey3003 <andre.d3003@gmail.com>
2021-02-20 19:13:18 +0100

    Show desktop view

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,6 @@ Rails.application.routes.draw do
 
   get '/cart/confirmation', to: 'cart#confirmation', as: 'confirmation'
 
-
-
   resources :items
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
